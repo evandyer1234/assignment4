c1f7f98923d239f479e19fe8e7e9b306d5e64410
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-09-22 14:29:03 -0400

    hey

---
+++ ARassingment4/assignment4/Assets/Scripts/robot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class robot : MonoBehaviour
+{
+    public Slider scale;
+    public Slider rotation;
+
+    public float size = 1f;
+    public float angle = 0f;
+
+    MakeAppearOnPlane MAON;
+    public GameObject manager;
+
+    void start()
+    {
+        MAON = manager.GetComponent<MakeAppearOnPlane>();
+    }
+    public void ScaleChange()
+    {
+        size = scale.value;
+        gameObject.transform.localScale = new Vector3(size, size, size);
+    }
+    public void DirectionChange()
+    {
+        angle = rotation.value;
+        gameObject.transform.localEulerAngles = new Vector3(0f, angle, 0f);
+    }
+    public void TogglePlane()
+    {
+        if (MAON != null)
+        {
+            MAON.enabled = !MAON.enabled;
+        }
+    }
+}
